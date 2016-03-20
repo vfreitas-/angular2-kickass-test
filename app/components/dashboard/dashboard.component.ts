550d4d817f7c0e1ad66012f8e4a2809100cee1e2
Vitor Freitas <vitor_freitas_b@hotmail.com>
2016-03-20 17:59:41 -0300

    Dashboard movies

--- app/components/dashboard/dashboard.component.ts
+++ app/components/dashboard/dashboard.component.ts
@@ -1,14 +1,27 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit, AfterViewInit} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {MoviesComponent} from '../movies/movies.component';
+
+declare var $:any;
 
 @Component({
 	selector: 'dashboard',
 	directives: [ROUTER_DIRECTIVES, MoviesComponent],
 	templateUrl: 'app/components/dashboard/dashboard.html'
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit {
+
+	public movieLists = [
+		{name: 'popular', title: 'Popular Movies'},
+		{name: 'upcoming', title: 'Upcoming'},
+		{name: 'toprated', title: 'Top Rated Movies'},
+		{name: 'released', title: 'Week Releases'}
+	];
 
 	constructor() {}
+
+	ngAfterViewInit() {
+		$('#movies_list').tabs();
+	}
 }
